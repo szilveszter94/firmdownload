f39e239d1600e78895012387c7c72377d6525b73
Szilveszter <test@gmail.com>
2023-08-28 21:23:03 +0200

    add more comments

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ import openpyxl
 
 # ------------------------------------------ HERE YOU CAN CHANGE CONDITIONS ------------------------------------ #
 
-# set the page, from which page you want to start the program
-FIRST_PAGE = 1
-# set the load times (second)
-PAGE_LOAD = 0.5
-FIRM_DETAIL_PAGE_LOAD = 0.1
-# items / page
-ITEMS_PER_PAGE = 25
-# here you must provide the Excel file name
-EXCEL_FILE_NAME = "email_by_name_0_9.xlsx"
-## here you must provide the base link, and after the '&page=' you must add a '{}' because it's a dynamic url
-BASE_LINK = "https://www.zoznam.sk/katalog/Spravodajstvo-informacie/Abecedny-zoznam-firiem/0-9/sekcia.fcgi?sid=1172&so=&page={}&desc=&shops=&kraj=&okres=&cast=&attr="
+FIRST_PAGE = 1  # set the first page, from which page you want to start the program
+LAST_PAGE = False  # provide last page if you don't want to loop through all pages
+PAGE_LOAD = 0.5  # set the load times (second)
+FIRM_DETAIL_PAGE_LOAD = 0.5  # set the load times (second)
+ITEMS_PER_PAGE = 25  # items / page
+EXCEL_FILE_NAME = "email_by_name_0_9.xlsx"  # here you must provide the Excel file name
+# here you must provide the base link, and after the '&page=' you must add a '{}' because it's a dynamic url
+BASE_LINK = "https://www.zoznam.sk/katalog/Spravodajstvo-informacie/Abecedny-zoznam-firiem/A/sekcia.fcgi?sid=1173&so=&page={}&desc=&shops=&kraj=&okres=&cast=&attr="
 
 # ---------------------------------------------------------------------------------------------------------------- #
 
@@ -37,13 +34,15 @@ try:
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div/div[3]/button[3]').click()
 except NoSuchElementException:
     pass
-page_number_string = driver.find_element(By.XPATH, f'/html/body/div[2]/div/div[2]/ul/li[4]/span/small').text
-page_number = int(page_number_string.strip("()"))
-correct_page_number = math.ceil(page_number / ITEMS_PER_PAGE)
-for page_num in range(FIRST_PAGE, correct_page_number + 1):  ##  the number of all pages (the +1 is because the loop run from 1 - e.g 24, but the 24 is not included)
+if not LAST_PAGE:
+    page_number_string = driver.find_element(By.XPATH, f'/html/body/div[2]/div/div[2]/ul/li[4]/span/small').text
+    page_number = int(page_number_string.strip("()"))
+    LAST_PAGE = math.ceil(page_number / ITEMS_PER_PAGE)
+for page_num in range(FIRST_PAGE,
+                      LAST_PAGE + 1):  # the number of all pages (the +1 is because the loop run from 1 - e.g 24, but the 24 is not included)
     driver.get(BASE_LINK.format(page_num))
     time.sleep(PAGE_LOAD)
-    for firm_num in range(1, ITEMS_PER_PAGE + 1):  ##26 the actual elements on a page (26 not included, so 25)
+    for firm_num in range(1, ITEMS_PER_PAGE + 1):  # 26 the actual elements on a page (26 not included, so 25)
         try:
             driver.find_element(By.XPATH,
                                 f'/html/body/div[3]/div/div[2]/div[2]/section/div[5]/div/ul/li[{firm_num}]/div/div[2]/h2/a').click()
