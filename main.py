14945083aaf3e799185fd9e9bca6db21622227fa
Szilveszter <test@gmail.com>
2023-08-28 20:10:39 +0200

    create base link

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ from selenium.webdriver.common.keys import Keys
 
 # give the chrome driver location (you can download it from "https://chromedriver.chromium.org/")
 chrome_driver_path = "D:/Games/chromedriver_win32/chromedriver.exe"
+## here you must provide the base link, and after the '&page=' you must add a '{}' because it's a dynamic url
+base_link = "https://www.zoznam.sk/katalog/Spravodajstvo-informacie/Abecedny-zoznam-firiem/0-9/sekcia.fcgi?sid=1172&so=&page={}&desc=&shops=&kraj=&okres=&cast=&attr="
 # use Chrome driver
 service_path = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service_path)
 # get the page
 emails = []
-driver.get(f"https://www.zoznam.sk/katalog/Spravodajstvo-informacie/Abecedny-zoznam-firiem/0-9/sekcia.fcgi?sid=1172&so=&page=1&desc=&shops=&kraj=&okres=&cast=&attr=")
+driver.get(base_link.format(1))
 time.sleep(1)
 try:
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div/div[3]/button[3]').click()
@@ -28,7 +30,7 @@ page_number_string = driver.find_element(By.XPATH, f'/html/body/div[2]/div/div[2
 page_number = int(page_number_string.strip("()"))
 correct_page_number = math.ceil(page_number / 25)
 for page_num in range(1, correct_page_number + 1): ##  the number of all pages (the +1 is because the loop run from 1 - e.g 24, but the 24 is not included)
-    driver.get(f"https://www.zoznam.sk/katalog/Spravodajstvo-informacie/Abecedny-zoznam-firiem/0-9/sekcia.fcgi?sid=1172&so=&page={page_num}&desc=&shops=&kraj=&okres=&cast=&attr=")
+    driver.get(base_link.format(page_num))
     time.sleep(1)
     for firm_num in range(1, 26): ##26 the actual elements on a page (26 not included, so 25)
         try:
